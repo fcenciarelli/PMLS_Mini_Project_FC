e7b88124e81955379b3a851e6769ffeb73af4c5a
Francesco Cenciarelli <francescocenciarelli@dhcp-10-240-100-7.cp.wireless.private.cam.ac.uk>
2023-11-30 19:41:18 +0000

    started implementing backprop in python and C

--- backprop_numpy.py
+++ backprop_numpy.py
@@ -1,1 +1,19 @@
 import numpy as np
+
+
+def FC_forward(x, w, b):
+
+    backprop_cache = (x, w, b)
+    x = x.reshape(x.shape[0], -1)
+    out = x.dot(w) + b
+    return out, backprop_cache
+
+
+
+x = np.random.randn(10, 5)
+w = np.random.randn(5, 3)
+b = np.random.randn(3)
+
+out, cache = FC_forward(x, w, b)
+
+print(out)
